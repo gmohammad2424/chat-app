fed6b74161efb9959a9b12ca1bd6f1d6678cf6dc
gmohammad2424 <gmohamad2424@gmail.com>
2025-04-29 18:10:41 +0330

    Update main.go

--- chat-app/backend/main.go
+++ chat-app/backend/main.go
@@ -521,7 +521,6 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 
 // Messages handler
 func messagesHandler(w http.ResponseWriter, r *http.Request) {
-    userID := r.Context().Value("user_id").(string)
     chatID := r.URL.Query().Get("chat_id")
     if chatID == "" {
         log.Println("chat_id parameter missing in /messages request")
